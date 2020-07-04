867dd65b7e388671d79c50f9ad3a801863f2e104
Marco Ataupillco <1130800@utp.edu.pe>
2020-07-04 16:50:52 -0500

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,20 +9,16 @@ export const environment = {
         region: 'us-west-2',
         UserPoolId: "us-west-2_oUDeQcE5u",
         ClientId: "1g9usum5nc2ohe2f9m8h7gq4bo",
-        
         identityPoolId: '',
         rekognitionBucket: 'rekognition-pics',
         albumName: "usercontent",
         bucketRegion: 'us-west-2',
-
         ddbTableName: 'LoginTrail', 
-
         cognito_idp_endpoint: '',
         cognito_identity_endpoint: '',
         sts_endpoint: '',
         dynamodb_endpoint: '',
         s3_endpoint: ''
-
     }    
 };
 
